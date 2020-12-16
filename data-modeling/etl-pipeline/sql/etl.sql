9f96ef376c4f7eded902ca56b0305130ab6125a1
Ai Tran <ai.tran@flightaware.com>
2020-12-15 20:12:22 -0600

    Update documentation and sql scripts

--- data-modeling/etl-pipeline/sql/etl.sql
+++ data-modeling/etl-pipeline/sql/etl.sql
@@ -17,7 +17,8 @@ SELECT distinct data->>'artist_id' as artist_id
             ELSE CAST(data->>'artist_longitude' AS real) 
           END as artist_longitude
 FROM song_data
-ORDER BY data->>'artist_id';
+ORDER BY data->>'artist_id'
+ON CONFLICT DO NOTHING;
 
 
 -- extract songs from song_data table
@@ -26,15 +27,16 @@ SELECT distinct data->>'song_id' as song_id
         , data->>'title' as title
         , data->>'artist_id' as artist_id
         , CASE 
-            WHEN data->>'duration' IS NULL THEN NULL 
-            ELSE CAST(data->>'duration' AS real) 
-          END as duration
-        , CASE 
             WHEN data->>'year' IS NULL THEN NULL 
             ELSE CAST(data->>'year' AS int) 
           END as year
+        , CASE 
+            WHEN data->>'duration' IS NULL THEN NULL 
+            ELSE CAST(data->>'duration' AS real) 
+          END as duration
 FROM song_data
-ORDER BY data->>'song_id';
+ORDER BY data->>'song_id'
+ON CONFLICT DO NOTHING;
 
 -- extract users from log_data table
 INSERT INTO users
@@ -42,28 +44,33 @@ SELECT distinct CAST(data->>'userId' AS int) as user_id
         , data->>'firstName' as first_name
         , data->>'lastName' as last_name
         , data->>'gender' as gender
-        , data->>'location' as location
+        , data->>'level' as level
 FROM log_data
-WHERE data->>'userId' != ''
-ORDER BY user_id;
+WHERE data->>'page' = 'NextSong'
+AND   data->>'userId' != ''
+ORDER BY user_id
+ON CONFLICT (user_id) DO NOTHING;
+--DO UPDATE SET (first_name, last_name, gender, level) = (EXCLUDED.first_name, EXCLUDED.last_name, EXCLUDED.gender, EXCLUDED.level);
+--TODO: unable to perform upsert on multiple entries here; need to figure out how to go around this issue;
 
 -- extract songplays from log_data table
-INSERT INTO songplays
-SELECT CASE 
-            WHEN data->>'sessionId' IS NULL THEN NULL 
-            ELSE CAST(data->>'sessionId' AS int) 
-          END as session_id
-        , data->>'level' as level
-        , data->>'ts' as start_time
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT  data->>'ts' as start_time
         , CASE
             WHEN TRIM(data->>'userId') = '' THEN NULL
             ELSE CAST(data->>'userId' as int)
           END as user_id
-        , artists.id as artist_id 
-        , songs.id as song_id
+        , data->>'level' as level
+        , songs.song_id
+        , artists.artist_id 
+        , CASE 
+            WHEN data->>'sessionId' IS NULL THEN NULL 
+            ELSE CAST(data->>'sessionId' AS int) 
+          END as session_id
+        , data->>'location'
         , data->>'userAgent' as user_agent
 FROM log_data
     LEFT JOIN artists ON log_data.data->>'artist' = artists.name
-    LEFT JOIN songs ON artists.id = songs.artist_id and trim(data->>'song') = songs.title
-;
+    LEFT JOIN songs ON artists.artist_id = songs.artist_id and trim(data->>'song') = songs.title
+WHERE data->>'page' = 'NextSong';
 
--- data-modeling/etl-pipeline/sql/verify_queries.sql
+++ data-modeling/etl-pipeline/sql/verify_queries.sql
@@ -7,3 +7,17 @@ SELECT COUNT(*) total_users FROM users;
 SELECT COUNT(*) total_songs_played FROM songplays;
 
 SELECT COUNT(*) = 1 as etl_passed FROM songplays WHERE song_id IS NOT NULL AND artist_id IS NOT NULL;
+
+SELECT sp.songplay_id
+        , sp.start_time, t.hour, t.day, t.week, t.month, t.year, t.weekday
+        , sp.user_id, u.first_name || ' ' || u.last_name as user_name
+        , sp.artist_id, a.name
+        , sp.song_id, s.title
+        , sp.session_id, sp.location, sp.user_agent 
+FROM songplays sp
+        left join time t on sp.start_time = t.start_time
+        left join users u ON sp.user_id = u.user_id 
+        left join artists a on sp.artist_id = a.artist_id 
+        left join songs s on sp.song_id = s.song_id
+WHERE   sp.song_id IS NOT NULL 
+AND     sp.artist_id IS NOT NULL;
